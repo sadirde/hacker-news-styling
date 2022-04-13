a698e0fe3bd0ff5de2bfa1a9c4f7345ce5330be3
sb <>
2022-04-13 04:22:57 +0200

    Adjusted style

--- hacker-news-styling.js
+++ hacker-news-styling.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://news.ycombinator.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Sadir
 // @description 4/11/2022, 9:30:31 AM
 // ==/UserScript==
@@ -15,8 +15,9 @@
   document.body.appendChild(style);
   style.sheet.insertRule("body, .title, .comment, .pagetop, .default, td { font-size: 1rem; }");
   style.sheet.insertRule(".subtext, .comhead, .yclinks { font-size: .8rem; }");
-  style.sheet.insertRule("#hnmain { background-color: rgba(255,255,255,.5); max-width: 50em; min-width: none; }");
-  style.sheet.insertRule("body { background-color: #f6f6ef; }");
+  style.sheet.insertRule("#hnmain { background-color: none; max-width: 50em; min-width: none; }");
+  style.sheet.insertRule("body { background-color: rgb(246, 246, 239); }");
   style.sheet.insertRule(".itemlist { padding: .3em .7em; }");
+  style.sheet.insertRule(".fatitem { background-color: #fff;  box-shadow: 0 1px 3px rgba(0, 0, 0, .2); width: 100%; }");
   
 })();
